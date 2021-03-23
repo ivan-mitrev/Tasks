a49c303f5820475cb40b32f4fe969a7d34d724e5
Ivan Mitrev <ivanizzle@gmail.com>
2021-03-23 13:09:37 +0200

    calculator

--- calculator.js
+++ calculator.js
@@ -14,16 +14,48 @@ class Calcumator {
 
     }
     appendNumber(number) {
-        this.curentOperant = number
+        if (number === '.' && this.curentOperant.includes('.')) return
+        this.curentOperant = this.curentOperant.toString() + number.toString()
     }
     chooseOperation(operation) {
-
+        if (this.curentOperant === '') return
+        if (this.previusOperant !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previusOperant = this.curentOperant
+        this.curentOperant = ''
     }
+    
     compute() {
+        let camputatiom
+        const prev = parseFloat(this.previusOperant)
+        const current = parseFloat(this.curentOperant)
+        if (isNan(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                camputatiom = prev + current
+                break
+            case '-':
+                camputatiom = prev - current
+                break
+            case '*':
+                camputatiom = prev * current
+                break
+            case '/':
+                camputatiom = prev / current
+                break
+            default:
+                return
+        }
 
+        this.curentOperant = camputatiom
+        this.operation = undefined
+        this.previusOperant = ''
     }
     updateDisplay() {
         this.curentText.innerText = this.curentOperant
+        this.previusText.innerText = this.previusOperant
     }
 }
 
@@ -32,7 +64,7 @@ const operationButtons = document.querySelectorAll(`[data-operation]`)
 const equealsButton = document.querySelector(`[data-equals]`)
 const deleteButton = document.querySelector(`[date-delete]`)
 const allclearButton = document.querySelector(`[data-all-clear]`)
-const previusText = document.querySelector(`[data-privius-operant]`)
+const previusText = document.querySelector(`[data-previus-operand]`)
 const curentText = document.querySelector(`[data-current-operand]`)
 
 const calcumator = new Calcumator(previusText, curentText)
@@ -42,4 +74,26 @@ numberButtons.forEach(button => {
         calcumator.appendNumber(button.innerText)
         calcumator.updateDisplay()
     })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calcumator.chooseOperation(button.innerText)
+        calcumator.updateDisplay()
+    })
+})
+
+equealsButton.addEventListener('click', button => {
+    calcumator.compute()
+    calcumator.updateDisplay()
+})
+
+allclearButton.addEventListener('click', button => {
+    calcumator.clear()
+    calcumator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calcumator.delete()
+    calcumator.updateDisplay()
 })
